06a427a8519362e2425de2b3cd9dbe0200507ae2
BraydenF <braydenf1616@gmail.com>
2017-12-27 21:12:46 -0600

    updated inheritance of owned

--- contracts/harambecoin.sol
+++ contracts/harambecoin.sol
@@ -2,11 +2,28 @@ pragma solidity ^0.4.16;
 
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
 
+contract owned {
+    address public owner;
+
+    function owned() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner {
+        owner = newOwner;
+    }
+}
+
+
 contract HarambeCoin is owned{
     // Public variables of the token
     string public name;
     string public symbol;
-    address public owner;
     uint8 public decimals = 0;
     uint256 public totalSupply;
 
@@ -34,25 +51,6 @@ contract HarambeCoin is owned{
         balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
         name = tokenName;                                   // Set the name for display purposes
         symbol = tokenSymbol;                               // Set the symbol for display purposes
-    }
-    
-    /*
-     *
-     */
-    function owned() {
-        owner = msg.sender;
-    }
-
-    /*
-     *
-     */
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address newOwner) onlyOwner {
-        owner = newOwner;
     }
 
     /**
