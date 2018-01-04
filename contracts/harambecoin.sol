b5cd9d3b8b8ab5c73dbc194e61753c2a570ba3a0
jmarcot <jtsm90401150@yahoo.com>
2018-01-03 20:57:31 -0600

    finished

--- contracts/harambecoin.sol
+++ contracts/harambecoin.sol
@@ -1,5 +1,8 @@
 pragma solidity 0.4.18;
 
+// imports the SafeMath.sol file
+//import './SafeMath.sol';
+
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
 
 contract owned {
@@ -199,7 +202,7 @@ contract HarambeCoin is owned{
      * @param _to The address receiving the coins
      * @param mintedAmount The amount of coin being minted
      */
-    function mintToken(address _to, uint256 mintedAmount) onlyOwner private {
+    function mintToken(address _to, uint256 mintedAmount) onlyOwner public {
         uint256 total = mintedAmount;
         balanceOf[_to] = balanceOf[_to].add(total);
         totalSupply = totalSupply.add(mintedAmount);
@@ -207,4 +210,37 @@ contract HarambeCoin is owned{
         Transfer(owner, _to, total);
     }
 
+}
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
 }
